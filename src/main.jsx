4cacae8883b0a94da68327e7fd03ef5533fe0a4a
shashi76 <usernameisp@users.noreply.github.com>
2026-04-06 17:57:52 +0530

    Add GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <CartProvider>
         <App />
       </CartProvider>
